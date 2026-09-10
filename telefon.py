27dc09fe21fd97a977e7db0fc6a63efab7a2aec7
“SillyKyubi” <itssakura06@gmail.com>
2026-09-10 12:09:22 +0200

    Steg 3 - Lagd en ny_venn som legger til i ordboka.

--- telefon.py
+++ telefon.py
@@ -11,8 +11,19 @@ telefonbok.append(person2)
 print("Hei, dette er telefonboka:")
 print(telefonbok)
 
+def legg_til():
+    navn = input("Hva er navnet til personen?")
+    nummer = input("Hva er nummeret deres?")
+
+    ny_venn = {"navn": navn, "nummer": nummer}
+    telefonbok.append(ny_venn)
+    print(f"{navn} ble lagt til i telefonboka!")
+
+
+#steg 2
 def vis_alle():
     for person in telefonbok:
         print(f"{person["navn"]}: {person["nummer"]}")
 
+legg_til()
 vis_alle()
