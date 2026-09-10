75adf308b32190e612c5b6fda24fd756dd9de6f2
SillyKyubi <itssakura06@gmail.com>
2026-09-10 11:44:13 +0200

    Add files via upload

---
+++ telefon.py
@@ -0,0 +1,17 @@
+telefonbok = []
+
+person1 = {
+    "navn": "Nora",
+    "nummer": "41326696"
+}
+
+person2 = {
+    "navn": "Edvin",
+    "nummer": "46403456"
+}
+
+telefonbok.append(person1)
+telefonbok.append(person2)
+
+print("Hei, dette er telefonboka:")
+print(telefonbok)
