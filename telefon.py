c3a36886b546b5c1ab4758cb5015cf90af0d57e7
“SillyKyubi” <itssakura06@gmail.com>
2026-09-10 11:50:38 +0200

    Steg 1 - Liste lagd med innhold.

--- telefon.py
+++ telefon.py
@@ -1,14 +1,9 @@
+#test
 telefonbok = []
 
-person1 = {
-    "navn": "Nora",
-    "nummer": "41326696"
-}
+person1 = {"navn": "Nora", "nummer": "41326696"}
 
-person2 = {
-    "navn": "Edvin",
-    "nummer": "46403456"
-}
+person2 = {"navn": "Edvin", "nummer": "46403456"}
 
 telefonbok.append(person1)
 telefonbok.append(person2)
